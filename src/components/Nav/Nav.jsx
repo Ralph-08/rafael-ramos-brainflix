b5190d3dec5bb6ef89c6dd34052e60f11004c441
Rafael Ramos <juniorr@rafaels-laptop.mynetworksettings.com>
2023-12-20 19:53:12 -0500

    added nav bar and media queries

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -7,17 +7,46 @@ import upload from "../../assets/icons/upload.svg";
 const Nav = () => {
   return (
     <nav className="nav">
-        <img className="nav__logo" src={logo} alt="brainflix logo" />
+      <img className="nav__logo" src={logo} alt="brainflix logo" />
+      <section className="nav__right-container">
         <ul className="nav__list">
-            <li className="nav__item">
-                <div className="nav__container">
-                    <img className="nav__search-icon" src={searchIcon} alt="search icon" />
-                    <input className="nav__input" type="text" placeholder="Search" />
-                </div>
-                <img className="nav__user-pic" src={userPic} alt="profile picture" />
-            </li>
+          <li className="nav__item">
+            <div className="nav__container">
+              <div className="nav__search-container">
+                <img
+                  className="nav__search-icon"
+                  src={searchIcon}
+                  alt="search icon"
+                />
+                <input
+                  className="nav__input"
+                  type="text"
+                  placeholder="Search"
+                />
+              </div>
+              <button className="nav__btn nav__btn--tablet">
+                <img
+                  className="nav__btn-icon nav__btn-icon--tablet"
+                  src={upload}
+                  alt="upload icon"
+                />
+                UPLOAD
+              </button>
+              <div className="nav__user-pic--container">
+                <img
+                  className="nav__user-pic"
+                  src={userPic}
+                  alt="profile picture"
+                />
+              </div>
+            </div>
+          </li>
         </ul>
-        <button className="nav__btn"><img src={upload} alt="upload icon" />UPLOAD</button>
+        <button className="nav__btn nav__btn--mobile">
+          <img className="nav__btn-icon" src={upload} alt="upload icon" />
+          UPLOAD
+        </button>
+      </section>
     </nav>
   );
 };
