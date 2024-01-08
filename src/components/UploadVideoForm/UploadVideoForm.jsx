5c2ca610ce5207c1d5874ac24b3fc48c0f45ad65
Rafael Ramos <juniorr@rafaels-laptop.mynetworksettings.com>
2024-01-07 23:26:09 -0500

    added media query for desktop

--- src/components/UploadVideoForm/UploadVideoForm.jsx
+++ src/components/UploadVideoForm/UploadVideoForm.jsx
@@ -7,29 +7,52 @@ const UploadVideoForm = () => {
   return (
     <section className="upload">
       <h2 className="upload__header">Upload Video</h2>
-      <p className="upload__subheader">VIDEO THUMBNAIL</p>
-      <img className="upload__thumbnail" src={thumbnailImg} alt="thumbnail image" />
       <form action="" className="form">
-        <label className="form__label">TITLE YOUR VIDEO</label>
-        <input
-          className="form__input input"
-          type="text"
-          placeholder="Add a title to your video"
-        />
-        <label className="form__label">ADD A VIDEO DESCRIPTION</label>
-        <textarea
-          className="form__textarea textarea"
-          type="text"
-          placeholder="Add a description to your video"
-        />
+        <div className="form__container">
+          <div className="form__flex-container">
+            <p className="form__subheader">VIDEO THUMBNAIL</p>
+            <img
+              className="form__thumbnail"
+              src={thumbnailImg}
+              alt="thumbnail image"
+            />
+          </div>
+          <div className="form__fields form__flex-contnainer">
+            <label className="form__label">
+              TITLE YOUR VIDEO
+              <input
+                className="form__input input"
+                type="text"
+                placeholder="Add a title to your video"
+              />
+            </label>
+            <label className="form__label">
+              ADD A VIDEO DESCRIPTION
+              <textarea
+                className="form__textarea textarea"
+                type="text"
+                placeholder="Add a description to your video"
+              />
+            </label>
+          </div>
+        </div>
+
         <div className="form__btns-container">
           <button className="form__btn form__btn--secondary">CANCEL</button>
           <div className="btn__container">
-            <img className="form__btn-icon" src={publishIcon} alt="publish icon" />
+            <img
+              className="form__btn-icon"
+              src={publishIcon}
+              alt="publish icon"
+            />
             <button className="form__btn form__btn--primary">
-            <img className="form__btn-icon--tablet" src={publishIcon} alt="publish icon" />
-                PUBLISH
-                </button>
+              <img
+                className="form__btn-icon--tablet"
+                src={publishIcon}
+                alt="publish icon"
+              />
+              PUBLISH
+            </button>
           </div>
         </div>
       </form>
